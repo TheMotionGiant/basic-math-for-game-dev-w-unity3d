0b83c0ed6297c2285903a50165b84a8b0c818160
TheMotionGiant <137898212+TheMotionGiant@users.noreply.github.com>
2023-06-29 14:55:37 +0200

    adding testpoint

--- Chapter-2-Examples/Assets/EX_2_3_MyScript.cs
+++ Chapter-2-Examples/Assets/EX_2_3_MyScript.cs
@@ -16,6 +16,10 @@ public class EX_2_3_MyScript : MonoBehaviour
     public float OverlapIntervalMax = float.NaN;        // Max/Min values of the overlap interval
     public float OverlapIntervalMin = float.NaN;
 
+    private GameObject testPoint;
+    private Vector3 testPointVector3;
+    public float TestPointY;
+
     static Color GreenColor = new Color(0.2f, 0.9f, 0.2f, 0.6f);
     static Color BlueColor = new Color(0.2f, 0.2f, 0.9f, 0.6f);
     static Color OverlapColor = new Color(0.2f, 0.9f, 0.9f, 0.9f);
@@ -38,6 +42,9 @@ public class EX_2_3_MyScript : MonoBehaviour
         OverlapInterval.DrawInterval = false; // Initially hide
         OverlapInterval.PositionToDraw = new Vector3(0.0f, 0, 0);  // One the axis
         OverlapInterval.IntervalColor =  OverlapColor;
+
+        testPoint = GameObject.Find("RedSphere");
+        testPointVector3 = testPoint.transform.position;
     }
 
     // Update is called once per frame
@@ -71,6 +78,8 @@ public class EX_2_3_MyScript : MonoBehaviour
             OverlapIntervalMin = float.NaN;
         }
 
+        testPointVector3.y = TestPointY;
+        testPoint.transform.position = testPointVector3;
     }
 }
 
